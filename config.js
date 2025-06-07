71ba49c88f3d792cc3c8a45458c38abb666c35e5
baonguyen20-05 <baobeo12b1@gmail.com>
2025-06-07 17:34:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Em vào xem đi,
+    có điều bất ngờ
+    cho em đấy`,
+    btnIntro: 'Anh ở đây',
+    title: 'Từng ngày yêu em',
+    desc: 'Yêu em rất nhiều',
+    btnYes: 'Vẫn cứ là Yêu anh ^^',
+    btnNo: 'Không nha anh:3',
+    question:'Thương anh từ thuở trăng tròn',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Bây giờ trăng khuyết vẫn còn thương anh <3',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Anh gọi em nhé, nhớ lắm rồi!',
+    btnAccept: 'Oke nha',
+    messLink: 'https://www.facebook.com/bao.huu.14661' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
